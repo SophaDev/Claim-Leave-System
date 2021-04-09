2eac7a956328a984b6f10165957f12384e12fbbd
Sopha Dev <sopha.kong@qwiq.io>
2021-04-09 10:28:39 +0700

    my project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-full h-screen mx-auto font-bold text-center text-blue-700 bg-blue-400">
+      Hello Homepage
+    </div>
+  );
+}
+
+export default App;
